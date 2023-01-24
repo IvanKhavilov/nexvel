e8968c81bd056a032be63e62f2a1e78a440f0225
IvanKhavilov <halilevivan@gmail.com>
2023-01-24 13:03:39 +0200

    pricesing

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function () {
     function updateTimer() {
       msLeft = endTime - +new Date();
       if (msLeft < 1000) {
-        element.innerHTML = "Time is up!";
+        element.innerHTML = "Час вийшов!";
       } else {
         time = new Date(msLeft);
         hours = time.getUTCHours();
@@ -25,4 +25,53 @@ $(function () {
   }
 
   countdown("ten-countdown", 10, 0);
+
+  $(".question__title").click(function (even) {
+    if ($(".question__inner").hasClass("question__inner--one")) {
+      $(".question__title").not($(this)).removeClass("active");
+      $(".question__body").not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
 });
+
+const tabs = document.getElementById("tabs");
+const contents = document.querySelectorAll(".program__content");
+
+const changeClass = el => {
+  for (let i = 0; i < tabs.children.length; i++) {
+    tabs.children[i].classList.remove("active");
+  }
+  el.classList.add("active");
+};
+
+tabs.addEventListener("click", e => {
+  const currTab = e.target.dataset.btn;
+  changeClass(e.target);
+
+  for (let i = 0; i < contents.length; i++) {
+    contents[i].classList.remove("active");
+    if (contents[i].dataset.content === currTab) {
+      contents[i].classList.add("active");
+    }
+  }
+});
+
+// const changeTab = el => {
+//   for (let i = 0; i < subTabs.children.length; i++) {
+//     subTabs.children[i].classList.remove("active");
+//   }
+//   el.classList.add("active");
+// };
+
+// subTabs.addEventListener("click", e => {
+//   const defines = e.target.dataset.btn;
+//   changeTab(e.target);
+
+//   for (let i = 0; i < subContents.length; i++) {
+//     subContents[i].classList.remove("active");
+//     if (subContents[i].dataset.content === defines) {
+//       subContents[i].classList.add("active");
+//     }
+//   }
+// });
